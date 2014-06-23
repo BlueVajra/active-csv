89c04eeaf807acf88a9ee40b391cc02d74528400
BlueVajra <cory.leistikow@gmail.com>
2014-06-23 11:39:26 -0600

    [#73730106] User can get information from csv file via methods

--- lib/active_csv/base.rb
+++ lib/active_csv/base.rb
@@ -2,6 +2,25 @@ require 'csv'
 
 module ActiveCSV
   class Base
+    def initialize(csv="")
+      @csv = csv
+      @file_path
+    end
 
+    def self.file_path
+      @file_path
+    end
+
+    def self.file_path=(path)
+      @file_path = path
+    end
+
+    def method_missing(method_name)
+      @csv[method_name.to_s] || super
+    end
+
+    def respond_to_missing?(method_name)
+      !@csv[method_name].nil? || super
+    end
   end
 end
